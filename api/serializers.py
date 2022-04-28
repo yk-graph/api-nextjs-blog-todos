68e1a6fbc2f5160b43513ffd0a76f951cf6ecf6e
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-28 16:58:54 +0900

    fix: serializer

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'white_only': True, 'required': True}}
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -16,12 +20,12 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = {'id', 'title', 'content', 'created_at'}
+        fields = ('id', 'title', 'content', 'created_at')
 
 
 class TaskSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
-        model = Post
-        fields = {'id', 'title', 'created_at'}
+        model = Task
+        fields = ('id', 'title', 'created_at')
